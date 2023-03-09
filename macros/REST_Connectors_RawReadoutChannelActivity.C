2de62e17fbba4b642a90e2096374b32662917f5d
DavidDiezIb <david101010@hotmail.com>
2023-03-09 11:30:00 +0100

    ID readout channel activity plot

--- macros/REST_Connectors_RawReadoutChannelActivity.C
+++ macros/REST_Connectors_RawReadoutChannelActivity.C
@@ -1,11 +1,13 @@
-#include <cstdio>
-#include <cstdlib>
-#include "TFile.h"
+//*******************************************************************************************************
+//*** Description: This macro receives as input two variable names that correspond to a file with the raw data
+//*** and the file with the readout.
+//*** It creates readout channel activity plots for X and Y directions and for AGET ID.
+//***
+//**********************************************************************************************************
 
 Int_t REST_Connectors_RawReadoutChannelActivity(string fName, TString fReadout) {
     //// Readout ////
-    TFile* f =
-        new TFile(fReadout);  //"/media/storage/home/davidp/git/trexdm-readouts/readoutsMM4S_2MM_v5.root"
+    TFile* f = new TFile(fReadout);
     TRestDetectorReadout* readout = NULL;
 
     // Search KEY of TRestDetectorReadout class.
@@ -24,6 +26,8 @@ Int_t REST_Connectors_RawReadoutChannelActivity(string fName, TString fReadout) 
                                        readout->GetReadoutModuleWithID(0)->GetModuleSizeX());
     TH1D* hChannelActivityY = new TH1D("", "Readout Y", readout->GetNumberOfChannels() / 2, 0,
                                        readout->GetReadoutModuleWithID(0)->GetModuleSizeY());
+    TH1D* hChannelActivityID =
+        new TH1D("", "Readout ID", readout->GetNumberOfChannels(), 0, readout->GetNumberOfChannels());
 
     TRestRun* run = new TRestRun(fName);
     TRestRawSignalEvent* fRawSignalEvent = new TRestRawSignalEvent();
@@ -35,6 +39,7 @@ Int_t REST_Connectors_RawReadoutChannelActivity(string fName, TString fReadout) 
 
         for (int k = 0; k < fRawSignalEvent->GetNumberOfSignals(); k++) {
             TRestRawSignal* sigA = fRawSignalEvent->GetSignal(k);
+            hChannelActivityID->Fill(sigA->GetID());
             double xA = readout->GetX(sigA->GetID());
             double yA = readout->GetY(sigA->GetID());
 
@@ -57,6 +62,10 @@ Int_t REST_Connectors_RawReadoutChannelActivity(string fName, TString fReadout) 
     TCanvas* cY1 = new TCanvas();
     hChannelActivityY->Draw("histo");
     hChannelActivityY->GetXaxis()->SetTitle("Y readout channel");
+
+    TCanvas* cID = new TCanvas();
+    hChannelActivityID->Draw("histo");
+    hChannelActivityID->GetXaxis()->SetTitle("ID readout channel");
 
     return 0;
 }
